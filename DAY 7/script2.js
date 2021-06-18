fc289378317250eb5371cf63e700fed5b5e3120c
Mannat-Sharma <85025909+Mannat-Sharma@users.noreply.github.com>
2021-06-18 16:37:37 +0530

    Create script2.js

---
+++ DAY 7/script2.js
@@ -0,0 +1,7 @@
+var student = { name : "David Rayy", sclass : "VI", rollno : 12 };
+
+console.log("Object before deleting the rollno property:",student) //printing the object before deleting the property
+
+delete student.rollno; //Deleting the rollno property of student
+
+console.log("Object after deleting the rollno property:",student) //printing the object after deleting the property

d62003a6f45ccd478bd36b4c9ae7959dc4d1ce59
Mannat-Sharma <85025909+Mannat-Sharma@users.noreply.github.com>
2021-07-03 23:29:11 +0530

    Update script.js

--- DAY 12/script.js
+++ DAY 12/script.js
@@ -1,1 +1,28 @@
 
+var input = document.querySelector("input");
+var addButton = document.querySelector(".add-btn");
+var todolist = document.querySelector(".todolist");
+
+addButton.addEventListener('click', () =>{
+    if(input.value){
+    todolist.innerHTML += '<li>'+input.value+'<span><i class="fas fa-check"></i><i class="fas fa-trash"></i></span></li>';
+    input.value ="";
+    }
+    else{
+        alert("⚠️ Add something as a TODO")
+    }
+})
+
+document.querySelector("ul").addEventListener('click', function(event){
+    if (event.target.className === "fas fa-trash"){
+        if (event.target.tagName === "I"){
+            event.target.parentElement.parentElement.remove();
+        }
+        if (event.target.tagName === "LI"){
+            event.target.parentElement.remove();
+        }
+    }
+    if (event.target.className === "fas fa-check"){
+        event.target.parentElement.parentElement.classList.toggle('completed');
+    }
+})

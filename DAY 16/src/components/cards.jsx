5cb75b69210e75df05211228a3c6439098db1246
Mannat-Sharma <85025909+Mannat-Sharma@users.noreply.github.com>
2021-07-09 12:18:26 +0530

    Create cards.jsx

---
+++ DAY 16/src/components/cards.jsx
@@ -0,0 +1,12 @@
+function Card () {
+    return (
+        <div className="card">
+            <p>*Declares a variable but doesn't use it*</p>
+            <p>THE IDE:</p>
+            <img src="https://i.imgflip.com/3ji1vg.jpg" alt="" />
+        </div>
+
+    );
+}
+
+export default Card;

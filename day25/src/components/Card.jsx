5864c35f24f580e3eddbb2a95940e369b434d054
Mannat <sharmamannat.25.11@gmail.com>
2021-08-06 22:21:07 +0530

    Day 25 Update

---
+++ day25/src/components/Card.jsx
@@ -0,0 +1,14 @@
+function Card(){
+    return(
+        <div class="card" style={{margin:"10px"}}>
+          <div class="card-body">
+            <blockquote class="blockquote mb-0">
+              <p>A well-known quote, contained in a blockquote element.</p>
+              <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+            </blockquote>
+          </div>
+        </div>
+    )
+}
+
+export default Card;

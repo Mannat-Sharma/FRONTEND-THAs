e3277bbd7936ac1ace5c2058612a6fd6838c8ede
Mannat <sharmamannat.25.11@gmail.com>
2021-10-03 13:52:46 +0530

    Day 35 Update

---
+++ day35/src/App.tsx
@@ -0,0 +1,34 @@
+import React, {useEffect} from 'react';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import "../node_modules/bootstrap/dist/js/bootstrap";
+import Navbar from './components/Navbar';
+import {useDispatch, useSelector} from 'react-redux';
+import updateUser from './actions';
+import Landing from './components/Landing';
+
+export interface PostType{
+  username: string;
+  post: string;
+}
+function App() {
+  const user = useSelector((state: any) => state.user);
+  const dispatch = useDispatch();
+  const [posts, setPosts] = React.useState<PostType[]>([]);
+  useEffect(() => {
+    dispatch(updateUser());
+    fetch("/data/post.json")
+    .then((res) => res.json())
+    .then((data) => {
+      setPosts(data);
+    })
+  }, []);
+  return (
+    <div className="App">
+     <Navbar/>
+     <Landing posts = {posts}/>
+    </div>
+  );
+}
+
+export default App;
